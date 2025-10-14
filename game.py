7de92fd7f5c0472d641e8447c4c0709b13add4b2
Olga Morgacheva <olga.morgacheva@icloud.com>
2025-10-14 22:41:31 +0300

    Обработка уже занятой ячейки

--- game.py
+++ game.py
@@ -1,15 +1,54 @@
+# game.py
+
 from gameparts import Board
+# Добавился ещё один импорт - исключение CellOccupiedError.
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 def main():
     game = Board()
-    game.display()
-    # Тут пользователь вводит координаты ячейки.
-    row = int(input('Введите номер строки: '))
-    column = int(input('Введите номер столбца: '))
-    # В метод make_move передаются те координаты, которые ввёл пользователь.
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
+    current_player = 'X'
+    running = True
     game.display()
 
+    while running:
+
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        game.make_move(row, column, current_player)
+        game.display()
+        current_player = 'O' if current_player == 'X' else 'X'
+
 if __name__ == '__main__':
-    main() 
+    main()
