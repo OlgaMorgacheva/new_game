b3eb962b838dee8f5f7ec2b3a6e90b1474029f93
Olga Morgacheva <olga.morgacheva@icloud.com>
2025-10-15 00:35:00 +0300

    Консольный вариант

---
+++ file_actions.py
@@ -0,0 +1,7 @@
+file = open('example.txt', 'r', encoding='utf-8')
+# Прочитать первые 12 символов из файла и сохранить их в переменную content.
+content = file.read(12)
+# Вывести на печать содержимое переменной content.
+print(content)
+# Закрыть файл.
+file.close() 
--- game.py
+++ game.py
@@ -1,54 +1,71 @@
 # game.py
 
 from gameparts import Board
-# Добавился ещё один импорт - исключение CellOccupiedError.
 from gameparts.exceptions import CellOccupiedError, FieldIndexError
+
+
+def save_result(result):
+    with open("results.txt", "a", encoding="utf-8") as file:
+        file.write(result + '\n')
+
+
 
 def main():
     game = Board()
-    current_player = 'X'
+    current_player = "X"
     running = True
     game.display()
 
     while running:
 
-        print(f'Ход делают {current_player}')
+        print(f"Ход делают {current_player}")
 
-        # Запускается бесконечный цикл.
         while True:
             try:
-                row = int(input('Введите номер строки: '))
+                row = int(input("Введите номер строки: "))
                 if row < 0 or row >= game.field_size:
                     raise FieldIndexError
-                column = int(input('Введите номер столбца: '))
+                column = int(input("Введите номер столбца: "))
                 if column < 0 or column >= game.field_size:
                     raise FieldIndexError
-                if game.board[row][column] != ' ':
-                    # Вот тут выбрасывается новое исключение.
+                if game.board[row][column] != " ":
                     raise CellOccupiedError
             except FieldIndexError:
                 print(
-                    'Значение должно быть неотрицательным и меньше '
-                    f'{game.field_size}.'
+                    "Значение должно быть неотрицательным и меньше "
+                    f"{game.field_size}."
                 )
-                print('Введите значения для строки и столбца заново.')
+                print("Введите значения для строки и столбца заново.")
                 continue
             except CellOccupiedError:
-                print('Ячейка занята')
-                print('Введите другие координаты.')
+                print("Ячейка занята.")
+                print("Пожалуйста, введите другие координаты.")
                 continue
             except ValueError:
-                print('Буквы вводить нельзя. Только числа.')
-                print('Введите значения для строки и столбца заново.')
+                print("Буквы вводить нельзя. Только числа.")
+                print("Введите значения для строки и столбца заново.")
                 continue
             except Exception as e:
-                print(f'Возникла ошибка: {e}')
+                print(f"Возникла ошибка: {e}")
             else:
                 break
 
         game.make_move(row, column, current_player)
         game.display()
-        current_player = 'O' if current_player == 'X' else 'X'
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            result = f"Победили {current_player}."
+            save_result(result)
+            print(result)
+            running = False
+        elif game.is_board_full():
+            result = "Ничья!"
+            save_result(result)
+            print(result)
+            running = False
 
-if __name__ == '__main__':
+        current_player = "O" if current_player == "X" else "X"
+
+
+if __name__ == "__main__":
     main()
